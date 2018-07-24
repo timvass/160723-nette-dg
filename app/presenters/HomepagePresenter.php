f2bcac3ba6a006d60082d122a6770a29a9084850
Timea Vass <timea.vass@zasilkovna.cz>
2018-07-24 10:14:49 +0200

    add password to form with js, in layout is the nette forms js

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -10,17 +10,24 @@ class HomepagePresenter extends Nette\Application\UI\Presenter
 	function createComponentSignForm() //Sign uppercase S
 	{
 		$form = new Nette\Application\UI\Form();
-		$form->addText('name', 'Jmeno');
+		$form->addText('name', 'Jmeno')
+		->setRequired('Zadejte prosim jmeno');
+
+		$form->addPassword('password', 'Heslo:')->setRequired('Zadejte prosim heslo');
+
 		$form->addSubmit('send', 'Registrovat');
+
+		$form->onSuccess[] = [$this, 'signFormSucceeded'];
+
 		return $form;
 	}
 
-	function xxx()
+/**	function xxx()
 	{
 		$form = $this->getComponent('signForm'); //sign lowecase s
 		$form = $this['signForm']; //syntactic sugar
 
 		$button = $form->getComponent('send');
 		$button = $form['send'];
-	}
+	}**/
 }
