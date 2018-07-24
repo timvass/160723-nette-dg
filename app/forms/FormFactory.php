edaa0ed80004aacbb3ea12779d6773f209ac0328
Timea Vass <timea.vass@zasilkovna.cz>
2018-07-24 16:07:22 +0200

    added bootstrap3 to our signin form

--- app/forms/FormFactory.php
+++ app/forms/FormFactory.php
@@ -16,6 +16,32 @@ class FormFactory
 	public function create()
 	{
 		$form = new Form;
+		$form->onRender[] = [$this, 'makeBootstrap3'];
 		return $form;
 	}
+
+	function makeBootstrap3(Form $form): void
+	{
+		$renderer = $form->getRenderer();
+		$renderer->wrappers['controls']['container'] = null;
+		$renderer->wrappers['pair']['container'] = 'div class=form-group';
+		$renderer->wrappers['pair']['.error'] = 'has-error';
+		$renderer->wrappers['control']['container'] = 'div class=col-sm-9';
+		$renderer->wrappers['label']['container'] = 'div class="col-sm-3 control-label"';
+		$renderer->wrappers['control']['description'] = 'span class=help-block';
+		$renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
+		$form->getElementPrototype()->class('form-horizontal');
+		foreach ($form->getControls() as $control) {
+			$type = $control->getOption('type');
+			if ($type === 'button') {
+				$control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-primary' : 'btn btn-default');
+				$usedPrimary = true;
+			} elseif (in_array($type, ['text', 'textarea', 'select'], true)) {
+				$control->getControlPrototype()->addClass('form-control');
+			} elseif (in_array($type, ['checkbox', 'radio'], true)) {
+				$control->getSeparatorPrototype()->setName('div')->addClass($type);
+			}
+		}
+	}
+
 }
