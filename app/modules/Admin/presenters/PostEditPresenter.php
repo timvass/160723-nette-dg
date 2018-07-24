ec87fa0cffdd73b8a9a22b35bcb2fc5890db7af6
Timea Vass <timea.vass@zasilkovna.cz>
2018-07-24 16:15:25 +0200

    add post

--- app/modules/Admin/presenters/PostEditPresenter.php
+++ app/modules/Admin/presenters/PostEditPresenter.php
@@ -2,8 +2,24 @@
 
 namespace App\Modules\Admin\Presenters;
 
+use \App\Forms\FormFactory;
+use Nette\Application\UI\Form;
 
 class PostEditPresenter extends BasePresenter
 {
+	/** @var FormFactory */
+	private $formFactory;
 
+	public function __construct(FormFactory $formFactory)
+	{
+		$this->formFactory = $formFactory;
+	}
+
+
+	/** @return Form */
+	protected function createComponentPostForm()
+	{
+		$form = $this->formFactory->create();
+		return $form;
+	}
 }
