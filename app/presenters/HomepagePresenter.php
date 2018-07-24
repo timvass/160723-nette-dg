b93e995b820df03b05eae20a79e2949e117bb870
Timea Vass <timea.vass@zasilkovna.cz>
2018-07-24 10:06:22 +0200

    add component form to a clear Nette web project

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -7,4 +7,20 @@ use Nette;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+	function createComponentSignForm() //Sign uppercase S
+	{
+		$form = new Nette\Application\UI\Form();
+		$form->addText('name', 'Jmeno');
+		$form->addSubmit('send', 'Registrovat');
+		return $form;
+	}
+
+	function xxx()
+	{
+		$form = $this->getComponent('signForm'); //sign lowecase s
+		$form = $this['signForm']; //syntactic sugar
+
+		$button = $form->getComponent('send');
+		$button = $form['send'];
+	}
 }
